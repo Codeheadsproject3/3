bc31fcd29d9b1b3b16ae94d7f820719d6ad9bd80
El Diablo <estesquefawer@gmail.com>
2020-07-29 23:24:17 -0500

    add picture route to front end

--- backend/routes/catch-api-routes.js
+++ backend/routes/catch-api-routes.js
@@ -1,5 +1,42 @@
 // Requiring our models
 var db = require("../models");
+const multer = require("multer");
+const path = require("path");
+
+// SET STORAGE ENGINE
+const storage = multer.diskStorage({
+  destination: "./public/uploads",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("myImage");
+
+function checkFileType(file, cb) {
+  // ALLOED FILE EXTENSIONS
+  const filetypes = /jpeg|jpg|png|gif/;
+
+  // CHECK FILE EXT.
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // CHECK MIME
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Only images: jpeg, jpg, gif & png !");
+  }
+}
 
 // Routes
 // =============================================================
@@ -14,7 +51,7 @@ module.exports = function(app) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.User
-    db.Post.findAll({
+    db.Catch.findAll({
       // where: {UserId: req.query.user_id},
       where: query,
       include: [db.User]
@@ -39,11 +76,35 @@ module.exports = function(app) {
   });
 
   // POST route for saving a new post(HERE GOES THE MULTER UPLOAD)
-  app.post("/api/catch", function(req, res) {
-    db.Catch.create(req.body).then(function(dbCatch) {
-      res.json(dbCatch);
+    app.get("/api/catch", (req, res) => res.render("index"));
+    app.post("/upload2", upload, (req, res) => {
+      console.log(req.file);
+      let filePath = "/uploads/" + req.file.filename;
+      db.File.create({ url: filePath }).then((dbFile) => res.json(dbFile));
     });
-  });
+    
+    app.post("/upload", (req, res) => {
+      upload(req, res, (err) => {
+        if (err) {
+          res.render("index", {
+            msg: err,
+          });
+        } else {
+          if (req.file == undefined) {
+            res.render("index", {
+              msg: "Error: No File Selected",
+            });
+          } else {
+            res.render("index", {
+              msg: "File Uploaded",
+              file: `uploads/${req.file.filename}`,
+            });
+          }
+        }
+      });
+    });
+};
+ 
 
   // DELETE route for deleting catch
   app.delete("/api/catch/:id", function(req, res) {
@@ -68,4 +129,3 @@ module.exports = function(app) {
       res.json(dbCatch);
     });
   });
-};
